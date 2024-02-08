5f4d89817198551a4637ec864afbb6e2ee2e45e6
alias1994 <aliasgary73@gmail.com>
2024-02-08 13:37:35 +0330

    add / route to server

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import './db.js'
 // routes
 import './routes/auth.js'
 
+app.get('/', async (req, res) => {
+  res.send('hello world')
+})
 
 app.listen(3000, () => {
   console.log(`Example app listening on port ${3000}`)
