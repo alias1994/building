85faf6a88e2afe532bfaa0f997f64851f5364f9d
unknown <mahdifb1381@gmail.com>
2024-02-09 02:04:00 +0330

    complete authenticate routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,42 +7,78 @@ const rand = () => Math.random().toString(36).substr(2);
 const tokenGenerator = () => rand() + rand()
 
 const addToken = async (id) => {
+    await Auth.deleteOne({userId: id})
     const token = tokenGenerator()
     const newToken = new Auth({ token, userId: id })
     await newToken.save()
-
     return token
 }
 
 app.post('/register', async (req, res) => {
-    const newUser = new User(req.body)
-
     try {
-        const { firstname, lastname, username, id } = await newUser.save()  
+        const newUser = new User(req.body)
+        await newUser.save()
+        const newToken = await addToken(newUser.id)
+        res.send({
+            type: "SIGNIN",
+            body: {
+                firstname: newUser.firstname,
+                lastname: newUser.lastname, 
+                username: newUser.username, 
+                token: newToken }
+        })
+
     } catch (error) {
-        console.log(error)
-        return res.send({
+        res.send({
             type: "ERROR",
-            body: { txt: 'username is duplicate' },
+            body: { txt: error },
         })
     }
 
-    const newToken = await addToken(id)
-    
-    res.send({
-        type: "SIGNIN",
-        body: { firstname, lastname, username, token: newToken },
-    })
 })
   
 app.post('/signin', async (req, res) => {
-    const user = await User.find(req.body)
-    console.log(user)
-  
-    res.send(user)
+    try {
+        const foundedUser = await User.findOne({
+            username : req.body.username, 
+            password: req.body.password
+        })
+        
+        const newToken = await addToken(foundedUser._id)
+
+        res.send({
+            type: "SIGNIN",
+            body: {
+                firstname: foundedUser.firstname,
+                lastname: foundedUser.lastname, 
+                username: foundedUser.username, 
+                token: newToken },
+        })
+
+    } catch (error) {
+        console.log("error: ",error);
+        res.send({
+            type: "ERROR",
+            body: { txt: error },
+        })       
+    }
 })
   
-app.get('/authenticate', async (req, res) => {
-    const ali = await User.find({ firstname: 'mahdi' }).exec()
-    res.send(ali)
+app.post('/authenticate', async (req, res) => {
+    const authedUser = await Auth.findOne({token: req.body.token })
+
+    if (authedUser){
+        const signedUser = await User.findById(authedUser.userId)
+        res.send({
+            type: "SIGNIN",
+            body: { token: req.body.token,
+                    firstname: signedUser.firstname,
+                    lastname: signedUser.lastname,
+                    username: signedUser.password }
+        })
+    } else {
+        res.send({ type: "SIGNOUT" })
+    }
+       
+    
 })
