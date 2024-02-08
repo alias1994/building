a475927995effc8aa44418ccaa4aec42154b1035
Ali <haghi.tarooni@gmail.com>
2024-02-08 15:28:10 -0800

    edit button and text fild by ali

--- frontend/src/component/custom-button.jsx
+++ frontend/src/component/custom-button.jsx
@@ -1,4 +1,39 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lime, purple ,orange} from '@mui/material/colors';
+import './custom-button.css'
+
+
+const DemoButton = styled(Button)(({ theme }) => ({
+    // width: 300,
+    maxWidth: '300px', maxHeight: '60px', minWidth: '300px', minHeight: '60px',
+    ...theme.typography.body2,
+    margin:"10px",
+    borderRadius: "20px",
+    backgroundColor: "#ED7662",
+    color: "#FFFFFF",
+    fontFamily:"vazir"
+  }))
+
+  const theme = createTheme({
+    palette: {
+      primary: lime,
+      secondary: orange,
+    },
+  });
+
 function CustomButton (name){
-    return(<h1>{name}</h1>)
+    return(
+    <ThemeProvider theme={theme}>
+      <DemoButton className='but' id='but1' variant="contained" color="secondary">{name}</DemoButton>
+    </ThemeProvider>
+)
 }
 export default CustomButton;
--- frontend/src/component/text-fild.jsx
+++ frontend/src/component/text-fild.jsx
@@ -1,4 +1,34 @@
-function TextFild (name){
-    return(<h1>{name}</h1>)
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import './text-fild.css'
+
+const DemoInput = styled(TextField)(({ theme }) => ({
+    width: 300,
+    ...theme.typography.body2,
+    borderRadius: "10px",
+    margin: "12px",
+    textAlign: 'right',
+    backgroundColor: '#F3F3F3',
+    disableUnderline: true,
+    display: 'inline-block',
+    "& fieldset": { border: 'none' },
+    "& label": {
+        width: "100%",
+        textAlign: "right",
+        transformOrigin: "center",
+        "&.Mui-focused": {
+            transformOrigin: "center"
+        }
+    }
+}))
+
+function TextFild(name) {
+    return (<Box><DemoInput className="inp" id="outlined-basic1" label={name} variant="outlined" InputLabelProps={{ style: { fontFamily: "vazir" , color:"#9E9E9E"} }} inputProps={{ style: { fontFamily: "vazir", textAlign: 'right'} }} /></Box>)
 }
 export default TextFild;
