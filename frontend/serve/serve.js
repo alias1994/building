5be2e4f22c0fe62052509ef335be32b77c16a310
alias1994 <aliasgary73@gmail.com>
2024-02-09 20:45:11 +0330

    change serve.js

--- frontend/serve/serve.js
+++ frontend/serve/serve.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static('../build'))
+app.use('/', express.static('../build'))
 
 app.listen(80, () => {
     console.log(`Example app listening on port ${80}`)
