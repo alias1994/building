03badc1b21e322ab720bd8ce02d9ac58b20e6e23
unknown <mahdifb1381@gmail.com>
2024-02-10 09:27:29 +0330

    complete error handling (txt) in auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ import getRandomValues from 'node:crypto'
 import app from '../app.js'
 import User from '../models/User.js'
 import Auth from '../models/Auth.js'
+import { log } from 'node:console';
 
 const rand = () => Math.random().toString(36).substr(2);
 const tokenGenerator = () => rand() + rand()
@@ -12,6 +13,24 @@ const addToken = async (id) => {
     const newToken = new Auth({ token, userId: id })
     await newToken.save()
     return token
+}
+
+const errorHandler = (e) => {
+    e = e.toString()
+    console.log(e);
+    if (e.includes("E11000") && e.includes("duplicate"))
+        return "این نام کاربری قبلا ثبت شده"
+    else if (e.matchAll(/(?<=`)\w+(?=` is required)/g)){
+        const o = {firstname: "نام" , lastname: "نام خانوادگی ", password: "رمز عبور"}
+        console.log(o.firstname);
+        const arr = e.match(/(?<=`)\w+(?=` is required)/g)
+        console.log(...arr);
+        let result = ""
+        arr.forEach(element => {
+            result += `${o[element]} نمی تواند خالی باشد \n`
+        });
+        return result
+    }
 }
 
 app.post('/register', async (req, res) => {
@@ -31,7 +50,7 @@ app.post('/register', async (req, res) => {
     } catch (error) {
         res.send({
             type: "ERROR",
-            body: { txt: error },
+            body: { txt: errorHandler(error)},
         })
     }
 
@@ -44,41 +63,47 @@ app.post('/signin', async (req, res) => {
             password: req.body.password
         })
         
-        const newToken = await addToken(foundedUser._id)
-
-        res.send({
-            type: "SIGNIN",
-            body: {
-                firstname: foundedUser.firstname,
-                lastname: foundedUser.lastname, 
-                username: foundedUser.username, 
-                token: newToken },
-        })
+        if (foundedUser){
+            const newToken = await addToken(foundedUser._id)
+            res.send({
+                type: "SIGNIN",
+                body: {
+                    firstname: foundedUser.firstname,
+                    lastname: foundedUser.lastname, 
+                    username: foundedUser.username, 
+                    token: newToken },
+            })
+        } else {
+            throw new Error("کابر یافت نشد، نام کاربری یا رمزعبور اشتباه است")
+        }
 
     } catch (error) {
         console.log("error: ",error);
         res.send({
             type: "ERROR",
-            body: { txt: error },
-        })       
+            body: { txt: error.toString() },
+        })
     }
 })
   
 app.post('/authenticate', async (req, res) => {
     const authedUser = await Auth.findOne({token: req.body.token })
-
-    if (authedUser){
-        const signedUser = await User.findById(authedUser.userId)
-        res.send({
-            type: "SIGNIN",
-            body: { token: req.body.token,
-                    firstname: signedUser.firstname,
-                    lastname: signedUser.lastname,
-                    username: signedUser.password }
-        })
-    } else {
-        res.send({ type: "SIGNOUT" })
+    try {
+        if (authedUser){
+            const signedUser = await User.findById(authedUser.userId)
+            res.send({
+                type: "SIGNIN",
+                body: { token: req.body.token,
+                        firstname: signedUser.firstname,
+                        lastname: signedUser.lastname,
+                        username: signedUser.username }
+            })
+        } else {
+            res.send({ type: "SIGNOUT" })
+        }   
+    } catch (error) {
+        console.log(error.toString());
     }
-       
+    
     
 })
